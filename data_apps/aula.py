e2c7e7f24d8ec51fb79d0f3e8fcc75ad290f64c8
stefane <stefaneadnas@gmail.com>
2023-07-02 19:02:35 -0300

    add pca

---
+++ data_apps/aula.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import datetime
+
+
+#radio button
+genre = st.radio(
+    "What\'s your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn\'t select comedy.")
+
